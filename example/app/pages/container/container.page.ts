71f9de676c6d8f84a33f1af1a6bcc003c64e3c84
yplarthur <arthur@yapla.be>
2023-10-26 07:51:58 +0200

    🔨 Angular 15 update

--- example/app/pages/container/container.page.ts
+++ example/app/pages/container/container.page.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { map, distinctUntilChanged, shareReplay } from 'rxjs/operators';
-import { UntypedFormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { Observable, BehaviorSubject } from "rxjs";
+import { map, distinctUntilChanged, shareReplay } from "rxjs/operators";
+import { UntypedFormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-container',
-  templateUrl: './container.page.html',
+  selector: "app-container",
+  templateUrl: "./container.page.html",
 })
 export class ContainerPage implements OnInit {
-
   constructor() {
     // make sure "this" works when called from zoid
     this.onButtonClicked = this.onButtonClicked.bind(this);
@@ -22,12 +21,14 @@ export class ContainerPage implements OnInit {
   public clicks = 0;
 
   ngOnInit() {
-    this.text$ = new BehaviorSubject<string>('World');
-    this.props$ = this.text$.pipe(
-      distinctUntilChanged(),
-      map((t) => ({ subject: t, onClick: this.onButtonClicked })),
-      shareReplay(1)
-    );
+    this.text$ = new BehaviorSubject<string>("World");
+    this.text$.subscribe(() => {
+      this.props$ = this.text$.pipe(
+        distinctUntilChanged(),
+        map((t) => ({ subject: t, onClick: this.onButtonClicked })),
+        shareReplay(1)
+      );
+    });
   }
 
   public onButtonClicked() {
@@ -39,6 +40,6 @@ export class ContainerPage implements OnInit {
   }
 
   onLoad(error?) {
-    console.log(`Widget loaded ${error ? 'with' : 'without'} error!`);
+    console.log(`Widget loaded ${error ? "with" : "without"} error!`);
   }
 }
